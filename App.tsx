6d8f986701f813fd3046d21b5bc57c07c50c0aa4
Avi Patel <avi.patel@hotmail.com>
2023-05-24 17:15:37 -0400

    add home screen

--- App.tsx
+++ App.tsx
@@ -10,11 +10,24 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import Home from './screens/Home'
 
+export type RootStackParamList = {
+  Home: undefined
+}
+
+const Stack = createNativeStackNavigator<RootStackParamList>()
+
 function App(): JSX.Element {
 
 
   return (
-    
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen 
+        name="Home"
+        component={Home}
+        options={{headerShown: false}}/>
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
--- screens/Home.tsx
+++ screens/Home.tsx
@@ -1,7 +1,11 @@
 import { View, Text, SafeAreaView } from 'react-native'
 import React from 'react'
+import { RootStackParamList } from '../App'
+import { NativeStackNavigationProp } from '@react-navigation/native-stack'
 
 type Props = {}
+
+export type NavigationProp = NativeStackNavigationProp<RootStackParamList, 'Home'>
 
 const Home = (props: Props) => {
   return (
